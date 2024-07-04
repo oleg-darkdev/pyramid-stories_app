8b7a800ad983c77439006f324b9ec6d88a2328c1
DarkDev <darkdev_brand@riseup.net>
2024-07-04 09:12:42 +0300

    add other boardgames data

---
+++ src/lib/shared/data/boardgamesList.js
@@ -0,0 +1,48 @@
+const boardgamesList = [
+	{
+		title: 'PALEO-STORIES',
+		img: '/images/boardgames/paleo-stories.svg',
+		link: '/',
+		anchor: '#paleo-stories',
+		desc: `Paleo Stories - educational boardgame, enhanced by the interactive web application, that introduces players to the world of paleontological expeditions and the science of paleontology. `
+	},
+	{
+		title: 'ARCHAEO-STORIES',
+		img: '/images/boardgames/archaeo-stories.svg',
+		link: 'https://archaeo-stories.vercel.app/',
+		anchor: '#archaeo-stories',
+		desc: `Archaeo stories - educational boardgame, enhanced by an interactive web application, offers you to explore ancient grounds, unearth artifacts, and  become a real archaeologist! `
+	},
+	{
+		title: 'HERITAGE-STORIES',
+		img: '/images/boardgames/heritage-stories.svg',
+		link: 'https://heritage-stories.vercel.app/',
+		anchor: '#heritage-stories',
+		desc: `Heritage stories - educational boardgame, enhanced by an interactive web application, about eco-friendly bicycle trip through the World Heritage Cities.`
+	},
+	{
+		title: 'CASTLE-STORIES',
+		img: '/images/boardgames/castle-stories.svg',
+		link: 'https://castle-stories.vercel.app/',
+		anchor: '#castle-stories',
+		desc: `Castle stories - educational boardgame, enhanced by an interactive web application, which will take you to the world of grand castles and ancient fortifications! Collect historical facts, complete tasks and unlock the secrets of ancient buildings to become a true connoisseur of medieval architecture.`
+	},
+	{
+		title: '12262-STORIES',
+		img: '/images/boardgames/12262-stories.svg',
+		link: 'https://12262-stories.vercel.app/',
+		anchor: '#12262-stories',
+		desc: `Explore the Kolskaya ultra-deep borehole, one of the deepest man-made facilities on the planet, drilled to a record depth of 12,262 meters. Discover a symbol of scientific and technical progress that has provided unique data for geological and geophysical research and generated many myths and legends and it's been called "the well to Hell."`
+	},
+	
+	{
+		title: 'Belarus/Poland-STORIES',
+		img: '/images/boardgames/by-pl-stories.svg',
+		link: 'https://by-pl.vercel.app/',
+		anchor: '#12262-stories',
+		desc: `"Cultures of Belarus and Poland" - through the materials prepared as part of this project, you will learn more about the relationship between these two neighboring countries, their history, culture and traditions.`
+	}
+];
+
+
+export default boardgamesList;
