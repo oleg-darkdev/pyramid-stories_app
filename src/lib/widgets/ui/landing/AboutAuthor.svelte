3e22fce602a426ea4757c239021bb85b2becbe7c
DarkDev <darkdev_brand@riseup.net>
2024-07-04 10:22:50 +0300

    add content, refactoring css classes

--- src/lib/widgets/ui/landing/AboutAuthor.svelte
+++ src/lib/widgets/ui/landing/AboutAuthor.svelte
@@ -1,8 +1,8 @@
-<section class="section-about background-color-blau">
+<section class="section-about background-color-blue pb-16">
     <div class="padding-global">
         <div class="container-main">
         <div class="padding-section-large no-margin-bottom">
-            <div class="info-box-fly-in background-color-creme text-color-blau">
+            <!-- <div class="info-box-fly-in background-color-creme text-color-blue">
                 <div class="margin-bottom margin-medium">
                     <h3 class="heading-style-h4">
                         Lorem Ipsum is simply 
@@ -10,40 +10,46 @@
                         dummy text of the printing 
                     </h3>
                 </div>
-
-                <!-- social icons -->
-            </div>
+            </div> -->
             <div class="margin-bottom margin-xhuge">
                 <div class="_12-columns-grid">
-                    <!-- check this id -->
-                    <div id="w-node-b389e26e-dc7a-8f7d-3a56-21096ff240f6-c0c55947">
+                    <div class="author-wrap">
                     <div class="margin-bottom margin-medium">
-                        <h2 class="heading-style-h1 text-color-creme">Hello! <br> I`am Oleg Medvedev </h2>
+                        <h2 class="heading-style-h1 text-color-creme">Hello! <br>    <a href='https://oleg-darkdev.vercel.app/' class='heading-style-h1 text-color-creme text-style-link' target="_blank" >I`am Oleg Medvedev</a> </h2>
                     </div>
                     <div class="margin-bottom margin-medium">
                         <h3 class="heading-style-h4 slideup-2">
-                                Lorem Ipsum is simply dummy text of the printing
-                            <span class="tm-text">
+
+                            <!-- <span class="tm-text">
                             <sup class="superscript">™</sup>
-                        </span>. 
-                        Ipsum has been the industry's standard dummy text ever since the 1500s
+                        </span>.  -->
+
                     </h3>
                     </div>
-                    <p class="text-size-regular columns-2 slideup-3">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        <br>
-                        <br>
-Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.                        <br>
-                        <span class="text-weight-bold">
-                            Lorem Ipsum is simply
-                        </span>
-Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.                    </p>
+                    <p class="text-size-regular slideup-3 mb-4">
+                        Product Web Engineer-IT Enthusiast, Gamification Coach, and JavaScript Programmer with across various technologies, <span class="text-weight-bold">4 years of experience in board game development and 3 years in web application development</span> including startups and open-source projects. See my open sorce projects are available on
+                        <span class="text-weight-bold"><a class='text-style-link ' href='https://github.com/oleg-darkdev' target="_blank">github profile.</a></span>
+                    </p>
+                    <p class="text-size-regular columns-2  slideup-3 mb-4">
+                        Engaged in merging the digital and physical worlds to create engaging and educational experiences. 
+                        <br> Сollaborated with several <span class="text-weight-bold"> NGOs and museums in Belarus and Poland </span> on various historical projects. 
+                    </p>
+                    <p class="text-size-regular slideup-3 mb-4">
+                        See my other historical projects and <span class="text-weight-bold"> educational board games that combine history and modern technologies in my <a class='text-style-link ' href='https://oleg-darkdev.vercel.app/' target="_blank"> portfolio.</a>
+                                        </span>
+                    </p>
+                    <p class="text-size-regular  slideup-3 mb-4">
+                        If you have ideas for collaboration - <span class="text-weight-bold">say me Hello from <a class='text-style-link ' href='https://www.linkedin.com/in/oleg-darkdev' target="_blank">linkedin.</a></span>
+                <br />
+                  In head with a love for technologies, in heart with a love for people 🖤 
+                    </p>
+
                         <div class="button-wrap">
                             <div class="margin-right margin-medium">
-                                <a href="/" class="button w-button">Lorem Ipsum is simply</a>
+                                <a href='https://oleg-darkdev.vercel.app/' target="_blank" class="button w-button">Get in touch</a>
                             </div>
-                            <a href="/" class="button infobox-open w-button">Lorem Ipsum is simply</a>
-                            <a href="/" class="button infobox-open-mobile w-button">Lorem Ipsum is simply</a>
+                            <!-- <a href="/" class="button infobox-open w-button">Lorem Ipsum is simply</a> -->
+                            <a href='https://oleg-darkdev.vercel.app/' target="_blank"class="button infobox-open-mobile w-button">Get in touch</a>
                         </div>
                     </div>
                     <!-- check this id -->
@@ -52,11 +58,11 @@ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, wh
                     </div>
                 </div>
             </div>
-            <div class="_12-columns-grid">
+            <!-- <div class="_12-columns-grid">
                 <div class="width-80--mobile">
-                    <!-- <h3 class="heading-style-h4">Hier noch ein paar Worte, die mich ganz gut beschreiben:</h3> -->
+                    <h3 class="heading-style-h4">Lorem Ipsum is simply</h3>
                 </div>
-            </div>
+            </div> -->
         </div>
         </div>
     </div>
@@ -66,6 +72,26 @@ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, wh
 <style>
 .author-img {
   grid-area: 1/8/2/12;
+}
+
+.author-wrap {
+  grid-area: 1/1/2/7;
+}
+.author-wrap {
+    grid-area: 1/1/2/7;
+}
+
+@media screen and (max-width: 479px) {
+  .author-wrap{
+    grid-area: 2/1/3/2;
+  }    
+}
+
+@media screen and (max-width: 991px) {
+
+  .author-wrap {
+    grid-area: 2/1/3/3;
+  }
 }
 
 @media screen and (max-width: 991px) {
