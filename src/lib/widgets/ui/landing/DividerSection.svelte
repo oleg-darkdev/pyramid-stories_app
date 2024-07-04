f2c480e8fce8766e79a06f6a2afc4f53e4c3939b
DarkDev <darkdev_brand@riseup.net>
2024-07-04 07:59:28 +0300

    upd content

--- src/lib/widgets/ui/landing/DividerSection.svelte
+++ src/lib/widgets/ui/landing/DividerSection.svelte
@@ -5,10 +5,12 @@
         <div class="testimonial-wrap slideup-1">
         <div>
             <div class="margin-bottom margin-medium">
-            <p id="w-node-da35277c-6b91-aaef-4082-c6e81c9cbb4c-c0c55947" class="text-size-regular">„Katie Caiger has been an absolute dream partner to have on our team; her guidance has been instrumental in helping us translate our new website and set up our new blog and social media accounts. We have been nothing but impressed by the amount of research, knowledge, enthusiasm and professionalism she brings to our project - not to mention her quick turn-around times. She is a very good listener and we always feel she really will do whatever it takes to turn our wishes into reality. We plan on keeping her onboard for as long as possible!“</p>
+            <p class="text-size-regular">
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer.
+</p>
             </div>
             <div>
-            <div class="heading-style-h6 text-color-gelb">Das Schloss �&nbsp;Berleburg �&nbsp;Team</div>
+            <div class="heading-style-h6 text-color-gelb">dummy text of the printing and typesetting industry.</div>
             </div>
         </div>
         </div>
