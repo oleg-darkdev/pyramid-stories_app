761b3bb75e0367ce4e4b1379c5422a7cc688df03
DarkDev <darkdev_brand@riseup.net>
2024-07-04 09:12:05 +0300

    intial app route

---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	// import {  } from '$widgets';
+	// import { } from '$entities';
+	// import {  } from '$shared';
+</script>
+
+
+<svelte:head>
+	<title>App - Puramids stories</title>
+</svelte:head>
