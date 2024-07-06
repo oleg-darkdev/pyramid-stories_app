304b035b1dcfac2ec70b83cec2e234bb266837c6
DarkDev <darkdev_brand@riseup.net>
2024-07-06 08:21:58 +0300

    upd content

--- src/lib/entities/ui/landing/ScenarioCard.svelte
+++ src/lib/entities/ui/landing/ScenarioCard.svelte
@@ -5,19 +5,22 @@
 
 <div class="info-box background-color-schwarz">
     <div class="margin-bottom margin-xlarge">
+    <div class="image-wrap">
+        <img src="{scenario.avatar}" loading="lazy" alt="scenario {scenario.title} avatar" class="image">
+    </div>
     <div class="margin-bottom margin-large">
-        <h3 class="heading-style-h4 {scenario.brandColor}">{scenario.title}</h3>
+        <!--  -->
+        <h3 class="heading-style-h4 text-center {scenario.textColor}">{scenario.title}</h3>
     </div>
     <div class="margin-bottom margin-small">
-        <div class="line background-color-creme"></div>
+        <div class="line {scenario.brandColor}"></div>
     </div>
     {#each scenario.desc as desc}
-        <p class="text-size-regular {scenario.brandColor}">
+    <!--  {scenario.textColor -->
+        <p class="text-size-regular}">
             {desc}
         </p>
     {/each}
     </div>
-    <div class="image-wrap">
-    <img src="{scenario.avatar}" loading="lazy" alt="scenario {scenario.title} avatar" class="image">
-    </div>
+
 </div>
