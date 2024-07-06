5212f3ccd91447499dc95ccf9197394afeffaa7c
DarkDev <darkdev_brand@riseup.net>
2024-07-07 00:45:45 +0300

    add text-color

--- src/lib/entities/ui/landing/ScenarioCard.svelte
+++ src/lib/entities/ui/landing/ScenarioCard.svelte
@@ -5,22 +5,21 @@
 
 <div class="info-box background-color-schwarz">
     <div class="margin-bottom margin-xlarge">
-    <div class="image-wrap">
-        <img src="{scenario.avatar}" loading="lazy" alt="scenario {scenario.title} avatar" class="image">
-    </div>
-    <div class="margin-bottom margin-large">
-        <!--  -->
-        <h3 class="heading-style-h4 text-center {scenario.textColor}">{scenario.title}</h3>
-    </div>
-    <div class="margin-bottom margin-small">
-        <div class="line {scenario.brandColor}"></div>
-    </div>
-    {#each scenario.desc as desc}
-    <!--  {scenario.textColor -->
-        <p class="text-size-regular}">
-            {desc}
-        </p>
-    {/each}
+      <div class="image-wrap">
+          <img src="{scenario.avatar}" loading="lazy" alt="scenario {scenario.title} avatar" class="image">
+      </div>
+      <div class="margin-bottom margin-large">
+          <!--  -->
+          <h3 class="heading-style-h4 text-center {scenario.textColor}">{scenario.title}</h3>
+      </div>
+      <div class="margin-bottom margin-small">
+          <div class="line {scenario.brandColor}"></div>
+      </div>
+      {#each scenario.desc as desc}
+          <p class="text-size-regular} {scenario.textColor}">
+              {desc}
+          </p>
+      {/each}
     </div>
 
 </div>
