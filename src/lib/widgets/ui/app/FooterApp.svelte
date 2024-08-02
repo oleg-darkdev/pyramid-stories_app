5162add8bdd1322e12e3be8a8d5c54eec6a17424
DarkDev <darkdev_brand@riseup.net>
2024-08-02 14:09:07 +0300

    intial app basic logic

---
+++ src/lib/widgets/ui/app/FooterApp.svelte
@@ -0,0 +1,25 @@
+
+<script>
+  export let screen;
+</script>
+
+<footer class='absolute bottom-0 w-full bg-black flex flex-row justify-center itemns-center py-4'>
+  <button class="mx-1" on:click={()=> screen = 0}>
+      <figure class="w-20">
+        <img src="/images/icons/pyramids.svg" alt='pyramids btn'  class='{screen == 0 ? 'bg-white': ''}'/>
+        <figcaption class='text-white '>Pyramids</figcaption>
+      </figure>
+  </button>
+  <button class="mx-1" on:click={()=> screen = 1}>
+      <figure class="w-20">
+        <img src="/images/icons/game.svg" alt='game btn'  class='{screen == 1 ? 'bg-white': ''}'/>
+        <figcaption class='text-white '>Play</figcaption>
+      </figure>
+  </button>
+  <button class="mx-1 " on:click={()=> screen = 2}>
+      <figure class="w-20 ">
+        <img src="/images/icons/manual.svg" alt='manual btn'  class='{screen == 2 ? 'bg-white': ''}'/>
+        <figcaption class='text-white '>Manual</figcaption>
+      </figure>
+  </button>
+</footer>
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,10 +1,87 @@
 <script>
-	// import {  } from '$widgets';
-	// import { } from '$entities';
-	// import {  } from '$shared';
+  import {FooterApp} from '$widgets';
+  import {ScenarioCardApp} from '$entities';
+  import {chinaScenario, pyramidsFullList, egyptScenario, indonesiaScenario, mesoamericaScenario, peruScenario, sudanScenario, scenariosFullList} from '$shared';
+
+  $: screen = 0;
 </script>
 
 
 <svelte:head>
 	<title>App - Puramids stories</title>
 </svelte:head>
+
+<div class="h-[85vh] overflow-y-scroll ">
+  {#if screen == 0}
+{#each scenariosFullList as scenario}
+<section class=" {scenario.brandColor}">
+    <div class="padding-global">
+        <div class="container-main">
+        <div class="padding-section-large w-full">
+
+            <div class='bg-black'>
+                <div class="margin-bottom margin-small  ">
+                    <h3 class="heading-style-h2 {scenario.textColor}">
+                        Pyramids from
+                        <br>
+                        {scenario.title} region
+                    </h3>
+                </div>
+            </div>
+
+          <div class="container-large">
+                <div class="">
+                  <div class="_3-columns-grid">
+                    {#each scenario.pyramids as scenario}
+                      <ScenarioCardApp {scenario}/>
+                    {/each}
+                  </div>
+                </div>
+              </div>
+        </div>
+      </div>
+    </div>
+</section>
+{/each}
+  {:else if screen == 1}
+    <section class="">
+        <div class="padding-global">
+          <div class="container-main">
+          <div class="padding-section-large w-full">
+            <div class='bg-black'>
+                  <div class="margin-bottom margin-small  ">
+                      <h3 class="heading-style-h2 text-white">
+                        Play boardgame
+                      </h3>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+  {:else if screen == 2}
+    <section class="">
+        <div class="padding-global">
+          <div class="container-main">
+          <div class="padding-section-large w-full">
+            <div class='bg-black'>
+                  <div class="margin-bottom margin-small  ">
+                      <h3 class="heading-style-h2 text-white">
+                        Manual
+                      </h3>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+
+  {/if}
+</div>
+
+<FooterApp bind:screen/>
+
+<style>
+
+
+</style>
